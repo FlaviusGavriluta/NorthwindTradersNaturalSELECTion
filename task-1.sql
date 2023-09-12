ebd8fa90b7e4dc12ec0332827a37f800b805e866
Flavius Gavriluta <116315036+FlaviusGavriluta@users.noreply.github.com>
2023-09-13 00:30:44 +0300

    Update task-1.sql

--- task-1.sql
+++ task-1.sql
@@ -1,6 +1,1 @@
-SELECT products.product_name  AS products,
-       suppliers.company_name AS company
-FROM products
-         INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id
-ORDER BY products ASC,
-         company ASC;
+-- YOUR SOLUTION HERE
