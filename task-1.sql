a39c76321e008081fb256bab180dd2e24c485b71
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 00:56:16 +0300

    task1

--- task-1.sql
+++ task-1.sql
@@ -1,1 +1,5 @@
--- YOUR SOLUTION HERE
+SELECT order_id,
+       to_char(shipped_date, 'YYYY.MM.DD') AS shipped_date
+FROM orders
+WHERE EXTRACT(YEAR FROM shipped_date) = 1996
+  AND EXTRACT(MONTH FROM shipped_date) = 8;
