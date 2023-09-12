42a3b1dd1c7d0c8647ca5aef1533d785bd1dac2b
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-13 02:00:18 +0300

    task7

--- task-7.sql
+++ task-7.sql
@@ -1,1 +1,5 @@
--- YOUR SOLUTION HERE
+SELECT DISTINCT ship_country
+FROM orders
+WHERE (EXTRACT(YEAR FROM order_date) BETWEEN 1997 AND 1998)
+  AND (ship_country ILIKE '%s%')
+ORDER BY ship_country;
