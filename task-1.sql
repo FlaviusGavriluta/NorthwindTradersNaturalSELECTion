0b0efd8c228a79376905c4128f4719bc04082f27
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-12 23:59:46 +0300

    task1

--- task-1.sql
+++ task-1.sql
@@ -1,1 +1,6 @@
--- YOUR SOLUTION HERE
+SELECT products.product_name  AS products,
+       suppliers.company_name AS company
+FROM products
+         INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+ORDER BY products ASC,
+         company ASC;
